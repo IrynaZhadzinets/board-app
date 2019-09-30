7f23d4de080a73733f803a61cd82449650816162
IrynaZhadzinets <iryna.zhadzinets@gmail.com>
2019-09-30 21:12:10 +0300

    feat: add angular/material

--- board/src/app/app.module.ts
+++ board/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BoardComponent } from './board/board.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { CardListComponent } from './card-list/card-list.component';
 import { CardItemComponent } from './card-item/card-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CardItemComponent } from './card-item/card-item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
